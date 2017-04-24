93d18fcaf7c8ff05cb119095185ba0a3bf3a173c
Matthew Mirus <mmirus@afphq.org>
2017-04-24 12:30:30 -0400

    Require ACF.

--- simple-members-only.php
+++ simple-members-only.php
@@ -13,8 +13,27 @@ namespace SMO;
 
 class SMO {
   function __construct() {
-    
+    // check if ACF is present and active, and fail if not
+		add_action('plugins_loaded', array($this, 'acf_check'));
   }
+  
+  // Check if Advanced Custom Fields is loaded and deactivate if not
+  public function acf_check() {
+    if(! class_exists('acf')) {
+      add_action('admin_init', array($this, 'deactivate'));
+      add_action('admin_notices', array($this, 'admin_notice_no_acf'));
+    }
+  }
+  
+  // deactivate this plugin
+  public function deactivate() {
+    deactivate_plugins(plugin_basename(__FILE__));
+  }
+  
+	// notify that ACF is required and plugin has been deactivated
+	public function admin_notice_no_acf() {
+		printf('<div class="error notice is-dismissible"><p class="extension-message"><strong>Advanced Custom Fields Pro</strong> is required by <strong>Simple Members Only</strong>. Deactivating the plugin.</p></div>');
+	}
 }
 
 new SMO();
