c635342d90e258809f3cdbb50df0198ad07ecb44
Matthew Mirus <mmirus@afphq.org>
2017-04-24 11:40:50 -0400

    Initial commit.

---
+++ simple-members-only.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Simple Members Only
+Plugin URI: https://github.com/mmirus/simple-members-only
+Description: Secure parts of your WordPress site for logged-in users only.
+Author: Matt Mirus
+Author URI: https://github.com/mmirus
+Version: 1.0
+GitHub Plugin URI: https://github.com/mmirus/simple-members-only
+*/
+
+namespace SMO;
+
+class SMO {
+  function __construct() {
+    
+  }
+}
+
+new SMO();
